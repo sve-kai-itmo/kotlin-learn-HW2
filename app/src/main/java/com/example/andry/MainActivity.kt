31baec8881f3d57bf886cc7b94fa6de12331a817
root <sve.ru@ya.ru>
2021-02-21 12:22:00 +0300

    version 2

--- app/src/main/java/com/example/andry/MainActivity.kt
+++ app/src/main/java/com/example/andry/MainActivity.kt
@@ -2,6 +2,11 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.wolfram.alpha.WAEngine
+ import com.wolfram.alpha.WAPlainText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +14,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.topAppBar))
+
+        val questionInput = findViewById<TextView>(R.id.QuestionInput)
+        val searchButton = findViewById<Button>(R.id.search_button)
+
+        searchButton.setOnClickListener {
+            askWolfram(questionInput.text.toString())
+        }
     }
+
+    fun askWolfram(question: String) {
+        val wolframAppId = "KEY" //KEY THAT WAS GENERATED ON WOLFRAM ALPHA
+        val engine = WAEngine()
+        engine.appID = wolframAppId
+        engine.addFormat("plaintext")
+
+        val query = engine.createQuery()
+        query.input = question
+
+        val answerText = findViewById<TextView>(R.id.answer_output)
+        answerText.text = "Let me think..."
+
+        Thread(Runnable{
+            val queryResult = engine.performQuery(query)
+            answerText.post {
+                if (queryResult.isError) {
+                    Log.e("wolfram error", queryResult.errorMessage)
+                    answerText.text = queryResult.errorMessage
+                } else if (!queryResult.isSuccess) {
+                    Log.e("wolfram error", "Sorry, I don't understand. Can you rephrase?")
+                    answerText.text = "Sorry, I don't understand. Can you rephrase?"
+                } else {
+                    for (pod in queryResult.pods) {
+                        if (!pod.isError) {
+                            for (subpod in pod.subpods) {
+                                for (element in subpod.contents) {
+                                    if (element is WAPlainText) {
+                                        Log.d("wolfram alpha", element.text)
+                                        answerText.text = element.text
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }).start()
+
+    }
+
 }
