20281df9e06a283b4fd6b4fa925ec949dd6eb055
root <sve.ru@ya.ru>
2021-02-26 21:35:09 +0300

    non-working v3

--- app/src/main/java/com/example/andry/MainActivity.kt
+++ app/src/main/java/com/example/andry/MainActivity.kt
@@ -1,14 +1,25 @@
  package com.example.andry
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wolfram.alpha.WAEngine
- import com.wolfram.alpha.WAPlainText
+import com.wolfram.alpha.WAPlainText
+import java.io.File
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,12 +32,59 @@ class MainActivity : AppCompatActivity() {
         searchButton.setOnClickListener {
             askWolfram(questionInput.text.toString())
         }
+
+        val speakButton = findViewById<Button>(R.id.speak_button)
+        speakButton.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What do you want to know?")
+
+            try {
+                startActivityForResult(intent, 1)
+            } catch (a: ActivityNotFoundException) {
+                Toast.makeText(
+                        applicationContext,
+                        "Your device is not supported :(",
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        val answerOutput = findViewById<TextView>(R.id.answer_output)
+        val textToSpeech = TextToSpeech(this, TextToSpeech.OnInitListener {})
+        textToSpeech.language = Locale.US
+
+        var speechRequest = 0
+        findViewById<FloatingActionButton>(R.id.read_answer).setOnClickListener {
+            val answer = answerOutput.text.toString()
+            textToSpeech.speak(answer, TextToSpeech.QUEUE_ADD, null, speechRequest.toString())
+            speechRequest += 1
+        }
     }
 
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+         startActivityForResult(intent, 1)
+         if (requestCode == 1) {
+             if (resultCode == RESULT_OK && data != null) {
+                 val result: ArrayList<String>? = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                 val question: String? = result?.get(0)
+                 if (question != null) {
+                     findViewById<TextView>(R.id.QuestionInput).text = question
+                 }
+             }
+         }
+     }
+
     fun askWolfram(question: String) {
-        val wolframAppId = "KEY" //KEY THAT WAS GENERATED ON WOLFRAM ALPHA
+        val wolframAppId = "4978LH-K523EGT6VX" //KEY THAT WAS GENERATED ON WOLFRAM ALPHA
         val engine = WAEngine()
         engine.appID = wolframAppId
+        Log.d("Get API", wolframAppId)
         engine.addFormat("plaintext")
 
         val query = engine.createQuery()
